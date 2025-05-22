a001ecf2e749fb3a0c5715b6f3aa18092a81d77f
akinkat <akinkat1252@gmail.com>
2025-05-22 23:29:10 +0900

    Add:templates

--- exam/views.py
+++ exam/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View, generic
+
 
 # Create your views here.
+class MultipleChoiceQuestionsView(generic.TemplateView):
+    template_name = 'exam/exam.html'
